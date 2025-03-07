795464c6273757ad0054102bd777b5a134f0cd72
Darío González <dar007alex@gmail.com>
2025-03-07 09:39:19 -0600

    DonutTile Finished

--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -5,13 +5,15 @@ class DonutTile extends StatelessWidget {
  final String donutPrice;
  final dynamic donutColor;  //dynamic porque será de tipo Color y también usará []
  final String imageName;
+ final String donutStore;
  
  const DonutTile({
    super.key,
    required this.donutFlavor,
    required this.donutPrice,
    required this.donutColor,
-   required this.imageName,
+   required this.imageName, 
+   required this.donutStore,
  });
                  
  @override
@@ -53,15 +55,40 @@ class DonutTile extends StatelessWidget {
                   vertical: 12
                 ),
                 child: Image.asset(imageName),
-              )
-
+              ),
               //Donut Flavor
-
+              Text(donutFlavor, 
+                style: const TextStyle(
+                  fontWeight:FontWeight.bold, 
+                  fontSize: 18)), 
+              //Espacio entre texto
+              const SizedBox(
+                height: 4,
+              ),
+              //DonutStore
+              Text(donutStore,
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12,
+                    color: Colors.grey[600])),
+              //Espacio entre textos
+              const SizedBox(height: 4),
               //Love icon + add buton
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: 
+                  MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.favorite_border),
+                    Icon(Icons.add)
+                  ],
+                ),
+              )
 
             ],
           ),
-            ),
+        ),
    );
  }
 }
